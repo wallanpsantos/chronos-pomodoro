d7eacd81b7ea473ae9dbb86d24c07d33548fc130
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2026-01-29 23:37:48 -0300

    feat: Add task submission form and accompanying styles in `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,28 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p> 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
