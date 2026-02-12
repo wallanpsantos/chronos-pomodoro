83784910da3c2bd9899454a3ea3a6e774e472a25
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2026-02-12 14:43:49 -0300

    feat: Configure Vite server to use `127.0.0.1` and enforce strict port binding
    - Set server host to `127.0.0.1` to prevent DNS resolution and avoid VPN interference.
    - Enabled `strictPort` to enforce the use of the specified port (`5173`).

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,15 +1,18 @@
+import { Container } from '../../components/Container/Container.tsx';
 import { MainTemplate } from '../../templates/MainTemplate/MainTemplate.tsx';
 
 export function NotFound() {
   return (
     <>
       <MainTemplate>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquam ultricies, nunc nisl
-          aliquet nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nisl eget aliquam ultricies, nunc nisl aliquet
-          nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nisl eget aliquam ultricies, nunc nisl aliquet nisl, eget
-          aliquam nisl nisl eget nisl.
-        </p>
+        <Container>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquam ultricies, nunc nisl
+            aliquet nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nisl eget aliquam ultricies, nunc nisl aliquet
+            nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nisl eget aliquam ultricies, nunc nisl aliquet nisl,
+            eget aliquam nisl nisl eget nisl.
+          </p>
+        </Container>
       </MainTemplate>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Forçamos '127.0.0.1' para evitar que o SO tente resolver o nome 'localhost' via DNS.
+  // Isso impede que a VPN no host intercepte a requisição local como tráfego de rede externa,
+  // contornando o bloqueio da Proteção Contra Ameaças e reduzindo a latência.
+  server: {
+    host: '127.0.0.1', // Força o servidor a rodar no IP em vez de 'localhost'
+    port: 5173, // Mantém a porta padrão ou a de sua preferência
+    strictPort: true, // Se a porta 5173 estiver ocupada, ele não pula para outra
+  },
+});
